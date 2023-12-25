d8254ff930035ea886bbbd6d2538b32f427dc221
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2023-12-26 01:11:59 +0530

    syntax error fix

--- scripts/get_image_property.sh
+++ scripts/get_image_property.sh
@@ -5,6 +5,6 @@ source "$(dirname "${BASH_SOURCE[0]}")/common_libs.sh"
 json_data="$1"
 image_name="$2"
 property="$3"
-$prefix="$4"
+prefix="$4"
 
 get_property "$json_data" "$image_name" "$property" "$prefix"
